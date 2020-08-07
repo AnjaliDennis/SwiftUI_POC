d705134d3be68ebe4cd5fffdf15dcd04cb530343
Anjali Dennis <anjalidennis13@gmail.com>
2020-08-08 01:04:02 +0530

    Initial Commit

---
+++ MapPOC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapPOC
+//
+//  Created by Anjali Pragati Dennis on 08/08/20.
+//  Copyright © 2020 Anjali Pragati Dennis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
